cfb10d1a064d05ddd2028b60e531dfd4d3c39072
daljeet1995 <ds025272@gmail.com>
2018-11-26 12:50:46 +0530

    second changes

--- rest.php
+++ rest.php
@@ -1,14 +1,27 @@
 <?php
+    require_once('constant.php');
+
   CLASS Rest{
+        protected $request;
+        protected $serviceName;
+        protected $param;
 
     public function __construct(){
-        
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            $this->throwError(REQUEST_METHOD_NOT_VALID,'Method is not post.');
+        }
       $handler = fopen('php://input','r');
-      $request = stream_get_contents($handler);
+      $this->request = stream_get_contents($handler);
+      $this->validateRequest();
     }
 
     public function validateRequest(){
-
+      if($_SERVER['CONTENT_TYPE'] !== 'application/json'){
+          $this->throwError(REQUEST_CONTENTTYPE_NOT_VALID,'Request content type is not valid'); 
+      }
+      
+       $data = json_decode($this->request, true);
+       print_r($data);
     }
 
     public function processApi(){
@@ -20,7 +33,9 @@
     }
 
     public function throwError($code, $message){
-        
+      header("content-type: application/json");  
+      $errorMsg =  json_encode(['error' => [ 'status'=> $code, 'message'=> $message ]]);
+       echo $errorMsg;exit;
     }
 
     public function returnResponse(){
